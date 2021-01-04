2ef0894be4aa62247206c270d6c80d0346f1db36
Michael Steil <mist64@mac.com>
2021-01-04 17:04:25 +0100

    table based dispatch

--- rs-server/src/dispatch.rs
+++ rs-server/src/dispatch.rs
@@ -8,14 +8,25 @@ pub struct PrivateContext<'a> {
     pub stats: Option<&'a Stats>,
 }
 
+const DISPATCH_TABLE: &[(&[u8], fn(&PageId, Option<PrivateContext>) -> Option<Page>, usize, bool)] = &[
+    (b"00000*", super::login::create,    0, true),
+    (b"9",      super::login::create,    0, true),
+    (b"77",     super::user::create,     0, false),
+    (b"7*",     super::historic::create, 1, false),
+    (b"*",      super::stat::create,     0, false),
+];
+
 pub fn get_page(pageid: &PageId, private_context: PrivateContext) -> Option<Page> {
-    if pageid.page.starts_with("00000") || pageid.page == "9" {
-        super::login::create(pageid, Some(private_context))
-    } else if pageid.page == "77" {
-        super::user::create(pageid, None)
-    } else if pageid.page.starts_with('7') {
-        super::historic::create(&pageid.reduced_by(1), None)
-    } else {
-        super::stat::create(pageid, None)
+    for (mask, function, reduce, private) in DISPATCH_TABLE {
+        let matches = if *mask.last().unwrap() == b'*' {
+            let mask = std::str::from_utf8(&mask[0..mask.len() - 1]).unwrap();
+            pageid.page.starts_with(mask)
+        } else {
+            pageid.page == std::str::from_utf8(mask).unwrap()
+        };
+        if matches {
+            return function(&pageid.reduced_by(*reduce), if *private { Some(private_context) } else { None });
+        }
     }
+    return None;
 }
