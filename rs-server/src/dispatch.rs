278322304363757f4a32b1a4a26d37be77611e94
Michael Steil <mist64@mac.com>
2021-01-04 19:17:13 +0100

    cleanups

--- rs-server/src/dispatch.rs
+++ rs-server/src/dispatch.rs
@@ -12,19 +12,21 @@ enum CanSeePrivateContext {
     Yes(fn(&PageId, PrivateContext) -> Option<Page>),
 }
 
-// * If a mask does not end in '*' or '-', the page number must match exactly.
-// * If a mask ends in '*', it only has to be a prefix of the page number.
-// * If a mask ends in '-', it only has to be a prefix of the page number. The prefix of the
-//   page number will be stripped when passed into the function.
-// * Only use CanSeePrivateContext::Yes for BTX-internal pages that need to access the
-//   user's info and statistics!
+// Mask:
+//   * If a mask does not end in '*' or '-', the page number must match exactly.
+//   * If a mask ends in '*', it only has to be a prefix of the page number.
+//   * If a mask ends in '-', it only has to be a prefix of the page number. The prefix of the
+//     page number will be stripped when passed into the function.
+// Function:
+//   * Only use CanSeePrivateContext::Yes for BTX-internal pages that need to access the
+//     user's info and statistics!
 // N.B.: The table must be in the right order: longer prefixes must come first!
 const DISPATCH_TABLE: &[(&[u8], CanSeePrivateContext)] = &[
-    (b"00000*", CanSeePrivateContext::Yes(super::login::create    )),
-    (b"9",      CanSeePrivateContext::Yes(super::login::create    )),
-    (b"77",     CanSeePrivateContext::No(super::user::create     )),
-    (b"7-",     CanSeePrivateContext::No(super::historic::create)),
-    (b"*",      CanSeePrivateContext::No(super::stat::create     )),
+    (b"00000*", CanSeePrivateContext::Yes(super::login::create)),
+    (b"9",      CanSeePrivateContext::Yes(super::login::create)),
+    (b"77",     CanSeePrivateContext::No (super::user::create)),
+    (b"7-",     CanSeePrivateContext::No (super::historic::create)),
+    (b"*",      CanSeePrivateContext::No (super::stat::create)),
 ];
 
 pub fn get_page(pageid: &PageId, private_context: PrivateContext) -> Option<Page> {
@@ -35,7 +37,10 @@ pub fn get_page(pageid: &PageId, private_context: PrivateContext) -> Option<Page
         if last == b'*' || last == b'-' {
             let mask = std::str::from_utf8(&mask[0..mask.len() - 1]).unwrap();
             matches = pageid.page.starts_with(mask);
-            reduce = if last == b'*' { 0 } else { mask.len() };
+            reduce = match last {
+                b'*' => 0,
+                _    => mask.len(),
+            };
         } else {
             matches = pageid.page == std::str::from_utf8(mask).unwrap();
             reduce = 0;
