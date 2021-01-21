1606bf1963b4b969e13599085b1a77afc24ce2fb
Michael Steil <mist64@mac.com>
2021-01-21 18:48:23 +0100

    cleanups

--- rs-server/src/top.rs
+++ rs-server/src/top.rs
@@ -5,21 +5,14 @@ extern crate termion;
 extern crate unicode_width;
 #[cfg(unix)]
 
-use html2text::render::text_renderer::{RichAnnotation, TaggedLine, TaggedLineElement};
-use std::{collections::HashMap, io::Read};
-use std::io::{self, Write};
-use termion::cursor::Goto;
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::screen::AlternateScreen;
-use unicode_width::UnicodeWidthStr;
+use html2text::render::text_renderer::{RichAnnotation, TaggedLineElement};
+use std::io::Read;
 
 fn to_style(tag: &Vec<RichAnnotation>) -> String {
     let mut style = String::new();
 
-    for ann in tag {
-        match *ann {
+    for annotation in tag {
+        match *annotation {
             RichAnnotation::Default => (),
             RichAnnotation::Link(_) => {
                 style.push_str(&format!("{}", termion::style::Underline));
@@ -62,78 +55,6 @@ fn to_style(tag: &Vec<RichAnnotation>) -> String {
         }
     }
     style
-}
-
-struct LinkMap {
-    lines: Vec<Vec<Option<String>>>, // lines[y][x] => Some(URL) or None
-}
-
-impl LinkMap {
-    pub fn link_at(&self, x: usize, y: usize) -> Option<&str> {
-        if let Some(ref linevec) = self.lines.get(y) {
-            if let Some(&Some(ref text)) = linevec.get(x) {
-                return Some(&text);
-            }
-        }
-        None
-    }
-}
-
-fn link_from_tag(tag: &Vec<RichAnnotation>) -> Option<String> {
-    let mut link = None;
-    for annotation in tag {
-        if let RichAnnotation::Link(ref text) = *annotation {
-            link = Some(text.clone());
-        }
-    }
-    link
-}
-
-fn find_links(lines: &Vec<TaggedLine<Vec<RichAnnotation>>>) -> LinkMap {
-    use self::TaggedLineElement::Str;
-
-    let mut map = Vec::new();
-    for line in lines {
-        let mut linevec = Vec::new();
-
-        for tli in line.iter() {
-            if let Str(ts) = tli {
-                let link = link_from_tag(&ts.tag);
-                for _ in 0..UnicodeWidthStr::width(ts.s.as_str()) {
-                    linevec.push(link.clone());
-                }
-            }
-        }
-
-        map.push(linevec);
-    }
-    LinkMap { lines: map }
-}
-
-struct FragMap {
-    start_xy: HashMap<String, (usize, usize)>,
-}
-
-fn find_frags(lines: &Vec<TaggedLine<Vec<RichAnnotation>>>) -> FragMap {
-    use self::TaggedLineElement::*;
-
-    let mut map = HashMap::new();
-    let mut y = 0;
-    for line in lines {
-        let mut x = 0;
-        for tli in line.iter() {
-            match tli {
-                FragmentStart(fragname) => {
-                    map.insert(fragname.to_string(), (x, y));
-                }
-                Str(ts) => {
-                    x += UnicodeWidthStr::width(ts.s.as_str());
-                }
-            }
-        }
-        y += 1;
-    }
-    FragMap { start_xy: map }
 }
 
 pub fn html2term(file: &mut impl Read) {
